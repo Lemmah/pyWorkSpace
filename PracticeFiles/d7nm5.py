4530dcdb53e3a1dc0ad1c9c373423d50305a03ce
Lemmah <lemayiannakolah@gmail.com>
2017-06-18 14:43:16 +0300

    Compute for a list of numbers divisible by 7 but not multiples of five in a given range.

---
+++ PracticeFiles/d7nm5.py
@@ -0,0 +1,22 @@
+
+def calcNum():
+    '''
+    A function which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+    between 2000 and 3200 (both included).
+    The numbers obtained are printed in a comma-separated sequence on a single line.
+    '''
+    # list of valid numbers which satisfy the conditions
+    validNum = []
+    # range of 2000 and 3200 both included
+    for num in range(1999, 3201):
+        divisibleBy7 = (num % 7 == 0)
+        multipleOf5 = (num % 5 == 0)
+        if divisibleBy7 and not multipleOf5:
+            validNum.append(num)
+        else:
+            continue
+
+    return validNum
+
+
+print(calcNum())
