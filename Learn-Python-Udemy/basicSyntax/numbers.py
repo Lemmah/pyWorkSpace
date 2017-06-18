3d4e14180d15e08119dcccde797ee3f39588d335
Lemmah <lemayiannakolah@gmail.com>
2017-06-18 17:38:51 +0300

    Working with numbers in py

---
+++ Learn-Python-Udemy/basicSyntax/numbers.py
@@ -0,0 +1,17 @@
+int_num = 10
+float_num = 10.2
+
+print(type(int_num))
+print(type(float_num))
+
+a = 10
+b = 23
+
+add = a + b
+print(add)
+sub = b - a
+print(sub)
+multi = a * b
+print(multi)
+div = b / a
+print(div)
