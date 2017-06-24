58d28fc006ddab753b8c2baebae5c58ca8053e7b
Lemmah <lemayiannakolah@gmail.com>
2017-06-24 22:41:26 +0300

    Regular, Class and Static methods in action.

--- OOP/employee.py
+++ OOP/employee.py
@@ -1,6 +1,11 @@
 # Employee blue print
 # Every method in a class takes the instance as the first argument
 # Self is the instance that works with all instances
+# Regular methods take the instance as the first argument
+# Class methods take the class as the first argument
+# Class methods can be used as alternative constructors
+# Static methods dont pass anything as the first argument, they are just any other functions included in the classes because they have a logical connection to our classes but do not actually depend on any specific instance.
+# If you're not using the class or instance in your function, then it is a good candidate for a static function
 
 class Employee:
     ''' This is the class that has employee attributes and behaviors '''
@@ -25,6 +30,40 @@ class Employee:
     def applyRaise(self):
         self.pay = int(self.pay * self.raiseAmount)
 
+    @classmethod
+    def setRaiseAmt(cls, amount):
+        cls.raiseAmount = amount
+
+    # Alternative constructor for a different usecase
+    @classmethod
+    def fromString(cls, empString):
+        first, last, pay = empString.split("-")
+        return cls(first, last, pay)
+
+    @staticmethod
+    def isWorkDay(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+
+# Static Method
+import datetime
+myDate = datetime.date(2017, 7, 12)
+print(Employee.isWorkDay(myDate))
+
+
+# Special use cases
+empString1 = "John-Doe-70000"
+empString2 = "Steve-Smith-3000"
+empString3 = "Jane-Doe-20000"
+
+# Using alternative constructors
+newEmployee1 = Employee.fromString(empString1)
+newEmployee2 = Employee.fromString(empString2)
+
+print(newEmployee1.fullName())
 
 print(Employee.numOfEmployees)
 
@@ -41,9 +80,10 @@ print(Employee.numOfEmployees)
 #emp_1.applyRaise()
 #print(emp_1.pay)
 
-# Looking at class variables
-# print(Employee.raiseAmount)
-# print(emp_1.raiseAmount)
+# Looking at class variables and class methods
+Employee.setRaiseAmt(1.05) # => class method
+print(Employee.raiseAmount) # => class variable
+print(emp_1.raiseAmount)
 
 # Printing namespaces
 # print(emp_1.__dict__)
