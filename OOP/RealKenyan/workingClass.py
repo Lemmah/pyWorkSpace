8efa3c76e8d7098117c759a1d3cc0d48085e2b97
Lemmah <lemayiannakolah@gmail.com>
2017-06-29 22:00:16 +0300

    Operator overloading in practice
    This is only on the working class OOP file. the rest are just irrelevant changes.

--- OOP/RealKenyan/workingClass.py
+++ OOP/RealKenyan/workingClass.py
@@ -15,11 +15,21 @@ class WorkingClass(KenyanCitizen):
         super().__init__(firstName, lastName, age, homeTown, monthlyExpenditure, phoneNumber)
         self.monthlyEarnings = monthlyEarnings
 
+    # Operator overloading in practice
+    def __add__(self, other):
+        ''' Get the net earnings of two employees by simply adding them together '''
+        if self.monthlyEarnings is not None and other.monthlyEarnings is not None:
+            return self.monthlyEarnings + other.monthlyEarnings
+        else:
+            return "One of them doesn't earn."
+
+# This will only get to run if this is the file being executed directly
 if __name__ == "__main__":
     Lemayian = WorkingClass("Regular", "Worker", 22, "Kajiado", 22000, "+254700613380")
-    Worker = WorkingClass.fromString("James - Lemayian - 20 - Narok - 1000 - Owns no phone")
+    Worker = WorkingClass.fromString("James - Lemayian - 50 - Narok - 1000 - Owns no phone")
     print(Lemayian)
     print(WorkingClass.totalExpenditures)
     print(WorkingClass.citizenStats())
+    print(Lemayian + Worker)
     # Looking at how the inheritance flows
     # help(WorkingClass)
